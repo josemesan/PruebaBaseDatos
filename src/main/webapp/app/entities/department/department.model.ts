2455b1e0ebd9ba20d3e94193a0513c02bea141f6
jomesan <mellado.proyecto2017@gmail.com>
2017-12-19 13:20:01 +0100

    yoJhipster2

---
+++ src/main/webapp/app/entities/department/department.model.ts
@@ -0,0 +1,8 @@
+import { BaseEntity } from './../../shared';
+
+export class Department implements BaseEntity {
+    constructor(
+        public id?: number,
+    ) {
+    }
+}
--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,11 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { PruebaBaseDatosDepartmentModule } from './department/department.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        PruebaBaseDatosDepartmentModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
